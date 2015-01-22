6a9442b127311fb605c132a3bcff4b34a6dd792c
Maurico Ivan <mauriciodinki@gmail.com>
2015-01-22 17:23:38 -0600

    añadido sevidor_suma

--- src/Cliente/Cilente_Suma.java
+++ src/Cliente/Cilente_Suma.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Cliente;
 
-/**
- *
- * @author mauricio
- */
+import java.net.*;
+import java.io.*;
+
 public class Cilente_Suma {
     
+    public static void main(String[] args) {
+    	
+    	
+    }
 }
--- src/Servidor/Servidor_Suma.java
+++ src/Servidor/Servidor_Suma.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servidor;
 
-/**
- *
- * @author mauricio
- */
+import java.net.*;
+import java.io.*;
+
+
 public class Servidor_Suma {
-    
+   
+   public static void main(String[] args) {
+    	int x,y,suma;
+    	String a,b;
+    	ServerSocket ss = null;
+
+    	try {
+    		ss = new ServerSocket(5051);
+    	} catch (IOException ioe){
+    		System.out.println(ioe);
+    	}
+
+    	try {
+    		System.out.println("En Espera De Un Numero");
+
+    		Socket s1 = ss.accept();
+
+    		InputStream is = s1.getInputStream();
+    		DataInputStream dis= new DataInputStream(is);
+    		a=dis.readUTF();
+
+    		System.out.println("El numeor recibido es " + a);
+    		System.out.println("En espera del segundo numero");
+
+    		Socket s2 = ss.accept();
+
+    		InputStream is2 = s2.getInputStream();
+    		DataInputStream dis2= new DataInputStream(is2);
+    		b=dis2.readUTF();
+
+
+    		System.out.println("El numero recibido es " + b);
+
+    		x = Integer.parseInt(a);
+    		y = Integer.parseInt(b);
+    		suma = x+y;
+
+    		System.out.println("El resultado de la suma es " + suma);
+
+    		s1.close();
+    		s2.close();
+
+    	} catch (IOException ioe2){
+    		System.out.println(ioe2);
+    	}
+    } 
 }
